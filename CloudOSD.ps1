2ec430197751d5a1a77b5d269f2c364668f47577
rnannenberg <116352417+rnannenberg@users.noreply.github.com>
2023-01-22 12:45:25 +0100

    Update CloudOSD.ps1

--- CloudOSD.ps1
+++ CloudOSD.ps1
@@ -29,7 +29,6 @@ Write-Host -ForegroundColor Green "Creating Scripts for OOBE phase"
 $OOBEcmdTasks = @'
 @echo off
 # Download and Install PowerShell 7
-#bitsadmin.exe /transfer Net7Download /download /priority normal https://github.com/PowerShell/PowerShell/releases/download/v7.3.1/PowerShell-7.3.1-win-x64.msi c:\windows\temp\PowerShell-7.3.1-win-x64.msi
 start /wait powershell.exe -NoL -ExecutionPolicy Bypass -C Invoke-WebRequest https://github.com/PowerShell/PowerShell/releases/download/v7.3.1/PowerShell-7.3.1-win-x64.msi -o C:\Windows\Temp\PowerShell-7.3.1-win-x64.msi
 msiexec.exe /i C:\Windows\Temp\PowerShell-7.3.1-win-x64.msi /qb-! /norestart REGISTER_MANIFEST=1 USE_MU=1 ENABLE_MU=1 ADD_PATH=1
 TITLE Setting-up OOBE phase
