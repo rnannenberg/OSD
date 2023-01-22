8b4f409389d6b81471b0d02b6971aaef5e33b391
rnannenberg <116352417+rnannenberg@users.noreply.github.com>
2023-01-22 19:36:37 +0100

    Update CloudOSD.ps1

--- CloudOSD.ps1
+++ CloudOSD.ps1
@@ -132,7 +132,7 @@ $appname = @(
 "*MicrosoftCorporationII.QuickAssist*"
 ) 
  # Remove apps for all users
- ForEach($app in $appname){ Get-AppxPackage -Allusers -Name $app | Remove-AppxPackage -ErrorAction SilentlyContinue | Out-File "c:\windows\temp\$(get-date -f yyyy-MM-dd)-RemoveApps.log" -force
+ ForEach($app in $appname){ Get-AppxPackage -Name $app | Remove-AppxPackage -ErrorAction SilentlyContinue | Out-File "c:\windows\temp\$(get-date -f yyyy-MM-dd)-RemoveApps.log" -force
          Write-Host -ForegroundColor DarkCyan "$app"
  } 
 Clear-Host 
