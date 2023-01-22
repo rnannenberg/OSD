6817cec800c8ec4fe5703c92cc0c8554e83ed27d
rnannenberg <116352417+rnannenberg@users.noreply.github.com>
2023-01-22 11:25:53 +0100

    Update CloudOSD.ps1

--- CloudOSD.ps1
+++ CloudOSD.ps1
@@ -43,7 +43,7 @@ $OOBEcmdTasks = @'
 @echo off
 # Download and Install PowerShell 7
 start /wait cmd.exe /c bitsadmin /transfer Net7Download /download /priority normal https://github.com/PowerShell/PowerShell/releases/download/v7.3.1/PowerShell-7.3.1-win-x64.msi c:\windows\temp\PowerShell-7.3.1-win-x64.msi
-start /wait cmd.ece /c msiexec.exe /package C:\Windows\Temp\PowerShell-7.3.1-win-x64.msi /quiet REGISTER_MANIFEST=1 USE_MU=1 ENABLE_MU=1 ADD_PATH=1
+start /wait cmd.exe /c msiexec.exe /package C:\Windows\Temp\PowerShell-7.3.1-win-x64.msi /quiet REGISTER_MANIFEST=1 USE_MU=1 ENABLE_MU=1 ADD_PATH=1
 TITLE Setting-up OOBE phase
 start /wait powershell.exe -NoL -ExecutionPolicy Bypass -F C:\Windows\Setup\Scripts\oobe.ps1
 del c:\Windows\Setup\scripts\oobe.*
