d509a2668962d893ed194e3f3c797923edcaaa27
rnannenberg <116352417+rnannenberg@users.noreply.github.com>
2023-01-22 08:17:46 +0100

    Update CloudOSD.ps1

--- CloudOSD.ps1
+++ CloudOSD.ps1
@@ -49,30 +49,36 @@ $OOBEcmdTasks | Out-File -FilePath 'C:\Windows\Setup\scripts\oobe.cmd' -Encoding
 $OOBEPS1Tasks = @'
 $Title = "OOBE installation/update phase"
 $host.UI.RawUI.WindowTitle = $Title
+
 # Change the ErrorActionPreference to 'SilentlyContinue'
-$ErrorActionPreference = 'SilentlyContinue' 
+$ErrorActionPreference = 'Continue'
+#$ErrorActionPreference = 'SilentlyContinue'
+
 # Set Environment
 Write-Host "Set Environment" -ForegroundColor Green
 $env:APPDATA = "C:\Windows\System32\Config\SystemProfile\AppData\Roaming"
 $env:LOCALAPPDATA = "C:\Windows\System32\Config\SystemProfile\AppData\Local"
-%env:TEMP = "C:\Windows\Temp"
 $Env:PSModulePath = $env:PSModulePath+";C:\Program Files\WindowsPowerShell\Scripts"
 $env:Path = $env:Path+";C:\Program Files\WindowsPowerShell\Scripts"
-# Register PS Modules
+
+# Register Powershell Modules and install tools
 Write-Host "Register PSGallery" -ForegroundColor Green
 Register-PSRepository -Name PSGallery -SourceLocation https://www.powershellgallery.com/api/v2/ -PublishLocation https://www.powershellgallery.com/api/v2/package/ -ScriptSourceLocation https://www.powershellgallery.com/api/v2/items/psscript/ -ScriptPublishLocation https://www.powershellgallery.com/api/v2/package/ -InstallationPolicy Trusted -PackageManagementProvider NuGet
-Write-Host "Install-Module PackageManagement" -ForegroundColor Green
+Write-Host "Install PackageManagement Module" -ForegroundColor Green
 Install-Module -Name PackageManagement | Out-Null
 $Error.Clear()
-Write-Host "Install-Module PowerShellGet" -ForegroundColor Green
+Write-Host "Install PowerShellGet Module" -ForegroundColor Green
 Install-Module -Name PowerShellGet | Out-Null
 Write-Host -ForegroundColor Green "Install OSD Module"
-Install-Module OSD | Out-Null
+Install-Module OSD -Force | Out-Null
 $Error.Clear()
 Write-Host -ForegroundColor Green "Install PSWindowsUpdate Module"
 Install-Module PSWindowsUpdate -Force | Out-Null
-Write-Host -ForegroundColor Green "Install WinSoftwareUpdate  Module"
-Install-Module -Name WinSoftwareUpdate -Scope AllUsers -Force | Out-Null
+Write-Host -ForegroundColor Green "Install WinGet Tools"
+Install-Module WingetTools -Force
+Write-Host -ForegroundColor Green "Install WinGet Module"
+Install-WinGet
+
 Write-Host -ForegroundColor Green "Remove Builtin Apps"
 # Create array to hold list of apps to remove 
 $appname = @( 
@@ -103,16 +109,15 @@ $appname = @(
 "*Clipchamp.Clipchamp*"
 "*Microsoft.GamingApp*"
 "*Microsoft.BingNews*"
+"*MicrosoftCorporationII.QuickAssist*"
 "*Microsoft.YourPhone*"
 "*MicrosoftTeams*"
-"*MicrosoftCorporationII.QuickAssist*"
 ) 
- # Remove apps for all users
- ForEach($app in $appname){ Get-AppxPackage -Name $app | Remove-AppxPackage -Allusers -ErrorAction SilentlyContinue | Out-File "c:\windows\temp\$(get-date -f yyyy-MM-dd)-RemoveApps.log" -force
+ # Remove apps from system
+ ForEach($app in $appname){ Get-AppxPackage -Name $app | Remove-AppxPackage -Allusers -ErrorAction SilentlyContinue 
          Write-Host -ForegroundColor DarkCyan "$app"
- } 
-Write-Host -ForegroundColor Green "Install Winget Updates"
-Update-RSWinSoftware | Out-File "c:\windows\temp\$(get-date -f yyyy-MM-dd)-Winget.log" -force
+ }
+ 
 Write-Host -ForegroundColor Green "Install .Net Framework 3.x"
 $Result = Get-MyWindowsCapability -Match 'NetFX' -Detail
 foreach ($Item in $Result) {
@@ -121,22 +126,45 @@ foreach ($Item in $Result) {
     }
     else {
         Write-Host -ForegroundColor DarkCyan "$($Item.DisplayName)"
-        $Item | Add-WindowsCapability -Online -ErrorAction Ignore | Out-File "c:\windows\temp\$(get-date -f yyyy-MM-dd)-WindowsCabability.log" -force
+        $Item | Add-WindowsCapability -Online -ErrorAction Ignore | Out-Null
     }
 }
+
+Write-Host -ForegroundColor Green "Microsoft .NET Windows Desktop Runtime 7"
+winget Install Microsoft.DotNet.DesktopRuntime.7
+Write-Host -ForegroundColor Green "Install Software Updates"
+Invoke-WGUpgrade -all
 Write-Host -ForegroundColor Green "Install Drivers from Windows Update"
 $UpdateDrivers = $true
 if ($UpdateDrivers) {
-    Install-WindowsUpdate -UpdateType Driver -AcceptAll -IgnoreReboot  | Out-File "c:\windows\temp\$(get-date -f yyyy-MM-dd)-DriversUpdate.log" -force
+    Install-WindowsUpdate -UpdateType Driver -AcceptAll -IgnoreReboot | Out-File "c:\windows\temp\$(get-date -f yyyy-MM-dd)-DriversUpdate.log" -force
 }
 Write-Host -ForegroundColor Green "Install Windows Updates"
 $UpdateWindows = $true
 if ($UpdateWindows) {
-    Write-Host -ForegroundColor DarkCyan 'Add-WUServiceManager -MicrosoftUpdate -Confirm:$false'
-    Add-WUServiceManager -MicrosoftUpdate -Confirm:$false  | Out-Null
-    Write-Host -ForegroundColor DarkCyan 'Install-WindowsUpdate -MicrosoftUpdate -AcceptAll -IgnoreReboot'
-    Install-WindowsUpdate -MicrosoftUpdate -AcceptAll -IgnoreReboot  | Out-File "c:\windows\temp\$(get-date -f yyyy-MM-dd)-WindowsUpdate.log" -force
+    Add-WUServiceManager -MicrosoftUpdate -Confirm:$false | Out-Null
+    Install-WindowsUpdate -MicrosoftUpdate -AcceptAll -IgnoreReboot | Out-File "c:\windows\temp\$(get-date -f yyyy-MM-dd)-WindowsUpdate.log" -force
 }
+
+#Install Winget Software Updates
+Write-Host -ForegroundColor Green "Install Application Updates"
+invoke-wgupgrade -All -IncludeUnknown
+Write-Host -ForegroundColor Green "Checking AssetTag"
+$AssetTag = (Get-WmiObject -Class Win32_SystemEnclosure | Select-Object SMBiosAssetTag).SMBiosAssetTag
+If ($null -eq $AssetTag){ 
+        Write-Host -ForegroundColor Green "Machine AssetTag is not set, Computer name not changed"
+    }
+    Else{
+        If ($AssetTag -eq "No Asset Tag"){
+            Write-Host -ForegroundColor Green "Virtual machine, No AssetTag available"
+        }
+        Else{
+            $MachineName = "DNB$AssetTag"
+            Write-Host -ForegroundColor Green "Machine name = $MachineName"
+            Rename-Computer -NewName $MachineName | Out-Null
+            }
+        }
+
 Write-Host -ForegroundColor Green "OOBE update phase ready, Restarting in 30 seconds!"
 Start-Sleep -Seconds 30
 Remove-Item C:\Drivers -Force -Recurse
