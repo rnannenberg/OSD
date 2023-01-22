0a70a5a5e101ffa1b168ab0c6979df8af6509db9
rnannenberg <116352417+rnannenberg@users.noreply.github.com>
2023-01-22 16:55:33 +0100

    Update CloudOSD.ps1

--- CloudOSD.ps1
+++ CloudOSD.ps1
@@ -32,7 +32,7 @@ $OOBEcmdTasks = @'
 start /wait powershell.exe -NoL -ExecutionPolicy Bypass -F C:\Windows\Setup\Scripts\ps.ps1
 start /wait msiexec.exe /i C:\Windows\Temp\PowerShell-7.3.1-win-x64.msi /qb-! /norestart REGISTER_MANIFEST=1 USE_MU=1 ENABLE_MU=1 ADD_PATH=1
 # Starting OOBE installation/update phase with PowerShell 7
-start /wait pwsh.exe -NoL -ExecutionPolicy Bypass -F C:\Windows\Setup\Scripts\oobe.ps1
+pwsh.exe -NoL -ExecutionPolicy Bypass -F C:\Windows\Setup\Scripts\oobe.ps1
 # Cleanup
 del c:\Windows\Setup\scripts\*.*
 exit 
@@ -74,6 +74,12 @@ $env:APPDATA = "C:\Windows\System32\Config\SystemProfile\AppData\Roaming"
 $env:LOCALAPPDATA = "C:\Windows\System32\Config\SystemProfile\AppData\Local"
 $Env:PSModulePath = $env:PSModulePath+";C:\Program Files\WindowsPowerShell\Scripts"
 $env:Path = $env:Path+";C:\Program Files\WindowsPowerShell\Scripts"
+#Define Icons
+    $CheckIcon = @{
+        Object          = [Char]8730
+        ForegroundColor = 'Green'
+        NoNewLine       = $true
+    }
 
 # Register Powershell Modules and install tools
 Write-Host "Register PSGallery" -ForegroundColor Green
@@ -160,12 +166,6 @@ foreach ($Item in $Result) {
     }
 }
 
-#Write-Host -ForegroundColor Green "Microsoft .NET Windows Desktop Runtime 7"
-#winget Install Microsoft.DotNet.DesktopRuntime.7
-
-Write-Host -ForegroundColor Green "Install Software Updates"
-Invoke-WGUpgrade -all
-
 #Install Driver updates
 Write-Host -ForegroundColor Green "Install Drivers from Windows Update"
 $UpdateDrivers = $true
@@ -181,27 +181,6 @@ if ($UpdateWindows) {
     Add-WUServiceManager -MicrosoftUpdate -Confirm:$false | Out-Null
     Install-WindowsUpdate -MicrosoftUpdate -AcceptAll -IgnoreReboot | Out-File "c:\windows\temp\$(get-date -f yyyy-MM-dd)-WindowsUpdate.log" -force
 }
-
-#Install Winget Software Updates
-#Write-Host -ForegroundColor Green "Install Application Updates"
-#invoke-wgupgrade -All -IncludeUnknown
-
-#Check AssetTag
-Write-Host -ForegroundColor Green "Checking AssetTag"
-$AssetTag = (Get-WmiObject -Class Win32_SystemEnclosure | Select-Object SMBiosAssetTag).SMBiosAssetTag
-If ($null -eq $AssetTag){ 
-        Write-Host -ForegroundColor Green "Machine AssetTag is not set, Computer name not changed"
-    }
-    Else{
-        If ($AssetTag -eq "No Asset Tag"){
-            Write-Host -ForegroundColor Green "Virtual machine, No AssetTag available"
-        }
-        Else{
-            $MachineName = "DNB$AssetTag"
-            Write-Host -ForegroundColor Green "Machine name = $MachineName"
-            Rename-Computer -NewName $MachineName | Out-Null
-            }
-        }
 
 Write-Host -ForegroundColor Green "OOBE update phase ready, Restarting in 30 seconds!"
 Start-Sleep -Seconds 30
