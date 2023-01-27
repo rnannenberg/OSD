e21b9a05526719b779454b70b6670e4d8a8c75f5
rnannenberg <116352417+rnannenberg@users.noreply.github.com>
2023-01-27 12:00:50 +0100

    Update CloudOSD.ps1

--- CloudOSD.ps1
+++ CloudOSD.ps1
@@ -163,14 +163,25 @@ $Env:PSModulePath = $env:PSModulePath+";C:\Program Files\WindowsPowerShell\Scrip
 $env:Path = $env:Path+";C:\Program Files\WindowsPowerShell\Scripts"
 Install-Module -Name PowerShellGet | Out-Null
 If ((Get-CimInstance -ClassName Win32_computersystem).model -eq "VMware Virtual Platform") {
-    write-host "Downlaod latest VMware tools" -ForegroundColor Green
+    write-host "Checking latest VMware tools" -ForegroundColor Green
+    $vmwareTools = "https://packages.vmware.com/tools/esx/latest/windows/x64/index.html"
+    $pattern = "[0-9]+\.[0-9]+\.[0-9]+\-[0-9]+\-x86_64"
     
+    #get the raw page content
+    $pageContent=(wget -UseBasicParsing -Uri $vmwareTools).content
     
-
-
-iex "& { $(irm https://aka.ms/install-powershell.ps1) } -UseMSI -Quiet"
+    #change one big string into many strings, then find only the line with the version number
+    $interestingLine = ($pageContent.split("`n") | Select-string -Pattern $pattern).tostring().trim()
+ 
+    #remove the whitespace and split on the assignment operator, then split on the double quote and select the correct item
+    $filename = (($interestingLine.Replace(" ","").Split("=") | Select-string -Pattern $pattern).ToString().Trim().Split("`""))[1]
+ 
+    $url = "https://packages.vmware.com/tools/esx/latest/windows/x64/$($filename)"
+    Write-Verbose "Downloading and installing $url"
+    iex "& { $(irm $url) } /S /v /qb REBOOT=R"
 '@
 $OOBEpsTasks | Out-File -FilePath 'C:\Windows\Setup\scripts\vm.ps1' -Encoding ascii -Force
+
 #================================================
 #  WinPE PostOS
 #  Bios.ps1
