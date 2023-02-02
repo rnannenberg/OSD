4b457643081f8a78e5b7bbcb4897e0ad13ead730
rnannenberg <116352417+rnannenberg@users.noreply.github.com>
2023-02-02 18:02:28 +0100

    Update CloudOSD.ps1

--- CloudOSD.ps1
+++ CloudOSD.ps1
@@ -427,7 +427,7 @@ Write-Host -ForegroundColor Green "Install Drivers from Windows Update"
 $Transcript = "$((Get-Date).ToString('yyyy-MM-dd-HHmmss'))-Drivers.log"
 Add-WUServiceManager -MicrosoftUpdate -Confirm:$false | Out-Null
 $driverupdates = Install-WindowsUpdate -UpdateType Driver -NotTitle "Preview" -AcceptAll -IgnoreReboot
-$resultdriverupdates = $driverupdates | Format-Table Result,Title -HideTableHeaders -Auto | Out-String -Width 100
+$resultdriverupdates = $driverupdates | Format-Table Result,Title -HideTableHeaders | Out-String
 Start-Sleep -Seconds 10
 
 Clear-Host
@@ -436,7 +436,7 @@ Write-Host -ForegroundColor Green "Install Windows Updates"
 $Transcript = "$((Get-Date).ToString('yyyy-MM-dd-HHmmss'))-Updates.log"
 Add-WUServiceManager -MicrosoftUpdate -Confirm:$false | Out-Null
 $softwareupdates = Install-WindowsUpdate -MicrosoftUpdate -NotTitle "Preview" -AcceptAll -IgnoreReboot
-$resultsoftwareupdates = $softwareupdates | Format-Table Result,Title -HideTableHeaders -Auto | Out-String -Width 100
+$resultsoftwareupdates = $softwareupdates | Format-Table Result,Title -HideTableHeaders | Out-String
 $ProgressPreference = 'SilentlyContinue'
 Start-Sleep -Seconds 10
 
