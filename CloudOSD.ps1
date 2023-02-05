78bb549c215f910d1f5284bdbc657fc8aa02ee26
rnannenberg <116352417+rnannenberg@users.noreply.github.com>
2023-02-05 16:08:16 +0100

    Update CloudOSD.ps1

--- CloudOSD.ps1
+++ CloudOSD.ps1
@@ -111,7 +111,7 @@ $env:Path = $env:Path+";C:\Program Files\WindowsPowerShell\Scripts"
 Install-PackageProvider -Name NuGet -MinimumVersion 2.8.5.201 -Force | Out-Null
 Install-Module -Name PowerShellGet -Force | Out-Null
 Start-Job -Name InstallPS -ScriptBlock {Invoke-Expression "& { $(Invoke-RestMethod 'https://aka.ms/install-powershell.ps1') } -UseMSI -Quiet"}
-$JobRC = Wait-Job -Name InstallPS -Timeout 120
+$JobRC = Wait-Job -Name InstallPS -Timeout 300
 If ($JobRC -eq "Completed") {
     Write-Host "PowerShell 7 installed" -ForegroundColor Green
     Start-Sleep -Seconds 5
