dd9b1900994b14c24514a78ee5bb3a1193d86188
rnannenberg <116352417+rnannenberg@users.noreply.github.com>
2023-02-05 16:30:23 +0100

    Update CloudOSD.ps1

--- CloudOSD.ps1
+++ CloudOSD.ps1
@@ -111,14 +111,15 @@ $env:Path = $env:Path+";C:\Program Files\WindowsPowerShell\Scripts"
 Install-PackageProvider -Name NuGet -MinimumVersion 2.8.5.201 -Force | Out-Null
 Install-Module -Name PowerShellGet -Force | Out-Null
 Start-Job -Name InstallPS -ScriptBlock {Invoke-Expression "& { $(Invoke-RestMethod 'https://aka.ms/install-powershell.ps1') } -UseMSI -Quiet"}
-$JobRC = Wait-Job -Name InstallPS -Timeout 300
-If ($JobRC -eq "Completed") {
+$JobRC = Wait-Job -Name InstallPS -Timeout 120
+Write-Host 
+If ($JobRC -eq "Completed") -or ($JobRC -eq "")  {
     Write-Host "PowerShell 7 installed" -ForegroundColor Green
     Start-Sleep -Seconds 5
 } 
   Else {
      Write-Host -ForegroundColor Red "Oops, something went wrong!"
-     Write=Host -ForegroundColor Red "The joberror was: $JobRC"
+     Write-Host -ForegroundColor Red "The joberror was: $JobRC"
      Write-Host -ForegroundColor Red "Lets reboot and try again!"
      Start-Sleep -Seconds 10
      Restart-Computer -Force
