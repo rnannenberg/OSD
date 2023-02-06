155f8318191e6832cc8a18e78269517d92a7fc04
rnannenberg <116352417+rnannenberg@users.noreply.github.com>
2023-02-06 14:47:19 +0100

    Update CloudOSD.ps1

--- CloudOSD.ps1
+++ CloudOSD.ps1
@@ -89,7 +89,7 @@ start /wait pwsh.exe -NoL -ExecutionPolicy Bypass -F C:\Windows\Setup\Scripts\VM
 # Check and change the Recovery settings
 start /wait pwsh.exe -NoL -ExecutionPolicy Bypass -F C:\Windows\Setup\Scripts\bios.ps1
 # Below a PS 7 session for debug and testing in system context, # when not needed 
-#start /wait pwsh.exe -NoL -ExecutionPolicy Bypass
+start /wait pwsh.exe -NoL -ExecutionPolicy Bypass
 start /wait pwsh.exe -NoL -ExecutionPolicy Bypass -F C:\Windows\Setup\Scripts\oobe.ps1
 exit 
 '@
@@ -242,13 +242,31 @@ $env:LOCALAPPDATA = "C:\Windows\System32\Config\SystemProfile\AppData\Local"
 $Env:PSModulePath = $env:PSModulePath+";C:\Program Files\WindowsPowerShell\Scripts"
 $env:Path = $env:Path+";C:\Program Files\WindowsPowerShell\Scripts"
 Install-Module -Name PowerShellGet -Force | Out-Null
-write-host "Downloading"
-Invoke-WebRequest -Uri $url -OutFile "C:\Windows\Temp\$filename"
-$params = "/install /passive /norestart"
-write-host "Installing"
-Start-Process -Wait -NoNewWindow -FilePath "C:\Windows\Temp\$filename" -ArgumentList $params
-Write-Host "Latest .Net Framework is installed" -ForegroundColor Green
-Start-Sleep -Seconds 5
+write-host "Downloading latest version"
+$job = Start-Job -ScriptBlock {Invoke-WebRequest -Uri $url -OutFile "C:\Windows\Temp\$filename"}
+if($job |Wait-Job -Timeout 300) {
+  if($job.State -eq 'Completed') {
+     write-host "Installing"
+     $params = "/install /passive /norestart"
+     Start-Process -Wait -NoNewWindow -FilePath "C:\Windows\Temp\$filename" -ArgumentList $params
+     Write-Host "Latest .Net Framework is installed" -ForegroundColor Green
+     Start-Sleep -Seconds 5       
+  }
+  else {
+     Write-Host -ForegroundColor Red "Oops, something went wrong!"
+     Write-Host -ForegroundColor Red "The error was: $job.State"
+     Write-Host -ForegroundColor Red "Lets reboot and try again!"
+     Start-Sleep -Seconds 10
+     Restart-Computer -Force    
+  }
+}
+#write-host "Downloading"
+#Invoke-WebRequest -Uri $url -OutFile "C:\Windows\Temp\$filename"
+#$params = "/install /passive /norestart"
+#write-host "Installing"
+#Start-Process -Wait -NoNewWindow -FilePath "C:\Windows\Temp\$filename" -ArgumentList $params
+#Write-Host "Latest .Net Framework is installed" -ForegroundColor Green
+#Start-Sleep -Seconds 5
 '@
 $OOBEnetTasks | Out-File -FilePath 'C:\Windows\Setup\scripts\net.ps1' -Encoding ascii -Force
 
