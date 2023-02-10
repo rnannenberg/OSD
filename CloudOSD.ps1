e0aac50006523585dd1641aff242bbbc11bd5d88
rnannenberg <116352417+rnannenberg@users.noreply.github.com>
2023-02-10 07:24:27 +0100

    Update CloudOSD.ps1

--- CloudOSD.ps1
+++ CloudOSD.ps1
@@ -396,16 +396,16 @@ If ((Get-CimInstance -ClassName Win32_BIOS).Manufacturer -eq "HP") {
    New-HPUWPDriverPack -Path "c:\drivers\uwp\" -UnselectList "Intel", "Nvidia", "Realtek", "Synaptics", "AMD"
    If ($? -eq "True") {
       $InstallScript = Get-ChildItem -Path "c:\drivers\uwp\" -Filter InstallAllApps.cmd -Recurse
-      Write-Host -ForegroundColor Green "Installing UWP Apps - $($InstallScript.FullName)"
+      Write-Host -ForegroundColor DarkGray "Installing HP UWP Apps - $($InstallScript.FullName)"
       Start-Process CMD.EXE -ArgumentList "/c $($InstallScript.FullName)" -Wait
    }
    Else {
-    Write-Host "No UWP Apps found for this machines"
+    Write-Host -ForegroundColor Yellow "No HP UWP Apps found for this machines"
    }
-   Write-Host -ForegroundColor Green "Searching for other apps for this machine"
+   Write-Host -ForegroundColor Green "Searching for other HP apps for this machine"
    $HPSoftPaq = Get-SoftpaqList -Category Software
    foreach ($HPPaq in $HPSoftPaq) {
-      Forech ($app in $HPappnames)	
+      foreach ($app in $HPappnames) {	
          If ($HPPaq.name -like $app) {
 	      $HPId = $HPPaq.id
 	      $HPName = $HPPaq.Name
@@ -413,11 +413,12 @@ If ((Get-CimInstance -ClassName Win32_BIOS).Manufacturer -eq "HP") {
 	      Write-Host -ForegroundColor Green "Downloading $HPid, $HPname with version $HPVersion"
 	      Get-Softpaq -Number $HPid.substring(2) -SaveAs "C:\Drivers\$HPId.exe" -Overwrite Yes
 	      Start-Process CMD.EXE -ArgumentList "/c $(C:\Drivers\$HPId.exe /s /f C:\Drivers\$HPid)" -Wait
+	  }
       } 
    }
 }
 Else {
-   Write-Host -ForegroundColor DarkGray "Not a HP machine"
+   Write-Host -ForegroundColor Yellow "This is not a HP machine"
 }
 Start-Sleep -Seconds 300
 '@
