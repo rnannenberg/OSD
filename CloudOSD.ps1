07e42f2b5e0ebccc7d4f14d7915baffc55094601
rnannenberg <116352417+rnannenberg@users.noreply.github.com>
2023-08-29 21:00:47 +0200

    Update CloudOSD.ps1

--- CloudOSD.ps1
+++ CloudOSD.ps1
@@ -129,8 +129,10 @@ start /wait powershell.exe -NoL -ExecutionPolicy Bypass -F C:\Windows\Setup\Scri
 Start-Sleep -Seconds 10
 # Download and Install PowerShell 7
 start /wait powershell.exe -NoL -ExecutionPolicy Bypass -F C:\Windows\Setup\Scripts\ps.ps1
+# Download and Install .Net Framework 6
+start /wait powershell.exe -NoL -ExecutionPolicy Bypass -F C:\Windows\Setup\Scripts\net6.ps1
 # Download and Install .Net Framework 7
-start /wait powershell.exe -NoL -ExecutionPolicy Bypass -F C:\Windows\Setup\Scripts\net.ps1
+start /wait powershell.exe -NoL -ExecutionPolicy Bypass -F C:\Windows\Setup\Scripts\net7.ps1
 # VcRedist Download and install supported versions
 start /wait pwsh.exe -NoL -ExecutionPolicy Bypass -F C:\Windows\Setup\Scripts\VcRedist.ps1
 # Check IF VM and install things
@@ -140,7 +142,7 @@ start /wait pwsh.exe -NoL -ExecutionPolicy Bypass -F C:\Windows\Setup\Scripts\bi
 # Download and install the HP UWP and other supporting apps (disabled some unwanted things)
 start /wait pwsh.exe -NoL -ExecutionPolicy Bypass -F C:\Windows\Setup\Scripts\appsuwp.ps1
 # Download and Install Microsoft 365
-start /wait powershell.exe -NoL -ExecutionPolicy Bypass -F C:\Windows\Setup\Scripts\m365.ps1
+# start /wait powershell.exe -NoL -ExecutionPolicy Bypass -F C:\Windows\Setup\Scripts\m365.ps1
 # Below a PS 7 session for debug and testing in system context, # when not needed 
 # start /wait pwsh.exe -NoL -ExecutionPolicy Bypass
 start /wait pwsh.exe -NoL -ExecutionPolicy Bypass -F C:\Windows\Setup\Scripts\oobe.ps1
@@ -276,7 +278,51 @@ $OOBEWiFiTasks | Out-File -FilePath 'C:\Windows\Setup\scripts\wifi.ps1' -Encodin
 
 #================================================
 #  WinPE PostOS
-#  net.ps1
+#  net6.ps1
+#================================================
+$OOBEnetTasks = @'
+$Title = "OOBE Latest .Net Framework 6.0 Download and install"
+$host.UI.RawUI.WindowTitle = $Title
+$ErrorActionPreference = 'SilentlyContinue'
+$ProgressPreference = "SilentlyContinue"
+$WarningPreference = 'SilentlyContinue'
+#Change URL and exe in code if new version is necessary
+#windowsdesktop-runtime-win-x64.exe
+#https://aka.ms/dotnet/6.0/windowsdesktop-runtime-win-x64.exe
+[Net.ServicePointManager]::SecurityProtocol = [Net.ServicePointManager]::SecurityProtocol -bor [Net.SecurityProtocolType]::Tls12
+[System.Net.WebRequest]::DefaultWebProxy.Credentials = [System.Net.CredentialCache]::DefaultCredentials
+Write-Host ".Net Framework 6 Download and install" -ForegroundColor Green
+$Transcript = "$((Get-Date).ToString('yyyy-MM-dd-HHmmss'))-Framework.log"
+$null = Start-Transcript -Path (Join-Path "C:\Windows\Temp" $Transcript ) -ErrorAction Ignore
+$env:APPDATA = "C:\Windows\System32\Config\SystemProfile\AppData\Roaming"
+$env:LOCALAPPDATA = "C:\Windows\System32\Config\SystemProfile\AppData\Local"
+$Env:PSModulePath = $env:PSModulePath+";C:\Program Files\WindowsPowerShell\Scripts"
+$env:Path = $env:Path+";C:\Program Files\WindowsPowerShell\Scripts"
+Install-Module -Name PowerShellGet -Force | Out-Null
+write-host "Downloading latest version"
+$job = Start-Job -ScriptBlock {Invoke-WebRequest -Uri https://aka.ms/dotnet/6.0/windowsdesktop-runtime-win-x64.exe -OutFile "C:\Windows\Temp\windowsdesktop6-runtime-win-x64.exe"}
+if($job |Wait-Job -Timeout 300) {
+  if($job.State -eq 'Completed') {
+     write-host "Installing"
+     $params = "/install /passive /norestart"
+     Start-Process -Wait -NoNewWindow -FilePath "C:\Windows\Temp\windowsdesktop6-runtime-win-x64.exe" -ArgumentList $params
+     Write-Host "Latest .Net 6 Framework is installed" -ForegroundColor Green
+     Start-Sleep -Seconds 5       
+  }
+  else {
+     Write-Host -ForegroundColor Red "Oops, something went wrong!"
+     Write-Host -ForegroundColor Red "The error was: $job.State"
+     Write-Host -ForegroundColor Red "Lets reboot and try again!"
+     Start-Sleep -Seconds 10
+     Restart-Computer -Force    
+  }
+}
+'@
+$OOBEnetTasks | Out-File -FilePath 'C:\Windows\Setup\scripts\net6.ps1' -Encoding ascii -Force
+
+#================================================
+#  WinPE PostOS
+#  net7.ps1
 #================================================
 $OOBEnetTasks = @'
 $Title = "OOBE Latest .Net Framework Download and install"
@@ -298,13 +344,13 @@ $Env:PSModulePath = $env:PSModulePath+";C:\Program Files\WindowsPowerShell\Scrip
 $env:Path = $env:Path+";C:\Program Files\WindowsPowerShell\Scripts"
 Install-Module -Name PowerShellGet -Force | Out-Null
 write-host "Downloading latest version"
-$job = Start-Job -ScriptBlock {Invoke-WebRequest -Uri https://aka.ms/dotnet/7.0/windowsdesktop-runtime-win-x64.exe -OutFile "C:\Windows\Temp\windowsdesktop-runtime-win-x64.exe"}
+$job = Start-Job -ScriptBlock {Invoke-WebRequest -Uri https://aka.ms/dotnet/7.0/windowsdesktop-runtime-win-x64.exe -OutFile "C:\Windows\Temp\windowsdesktop7-runtime-win-x64.exe"}
 if($job |Wait-Job -Timeout 300) {
   if($job.State -eq 'Completed') {
      write-host "Installing"
      $params = "/install /passive /norestart"
-     Start-Process -Wait -NoNewWindow -FilePath "C:\Windows\Temp\windowsdesktop-runtime-win-x64.exe" -ArgumentList $params
-     Write-Host "Latest .Net Framework is installed" -ForegroundColor Green
+     Start-Process -Wait -NoNewWindow -FilePath "C:\Windows\Temp\windowsdesktop7-runtime-win-x64.exe" -ArgumentList $params
+     Write-Host "Latest .Net 7 Framework is installed" -ForegroundColor Green
      Start-Sleep -Seconds 5       
   }
   else {
@@ -316,7 +362,7 @@ if($job |Wait-Job -Timeout 300) {
   }
 }
 '@
-$OOBEnetTasks | Out-File -FilePath 'C:\Windows\Setup\scripts\net.ps1' -Encoding ascii -Force
+$OOBEnetTasks | Out-File -FilePath 'C:\Windows\Setup\scripts\net7.ps1' -Encoding ascii -Force
 
 #================================================
 #  WinPE PostOS
